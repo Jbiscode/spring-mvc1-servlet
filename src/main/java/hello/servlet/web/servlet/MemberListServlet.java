3135d8c1afaf7709b0eedd4c77909b8adf66edc5
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-21 11:53:47 +0900

    [3-2] 서블릿으로 어플리케이션 만들기

--- src/main/java/hello/servlet/web/servlet/MemberListServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberListServlet.java
@@ -1,2 +1,59 @@
-package hello.servlet.web.servlet;public class MemberListServlet {
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "memberListServlet", urlPatterns = "/servlet/members")
+public class MemberListServlet extends HttpServlet {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Member> memberList = memberRepository.findAll();
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = resp.getWriter();
+
+//        StringBuilder sb = new StringBuilder();
+        writer.write("<html>\n");
+        writer.write("<head>\n");
+        writer.write("    <meta charset=\"UTF-8\">\n");
+        writer.write("    <title>Title</title>\n");
+        writer.write("</head>\n");
+        writer.write("<body>\n");
+        writer.write("    <a href=\"/index.html\">메인</a>\n");
+        writer.write("    <table>\n");
+        writer.write("        <thead>\n");
+        writer.write("            <th>id</th>\n");
+        writer.write("            <th>username</th>\n");
+        writer.write("            <th>age</th>\n");
+        writer.write("        </thead>\n");
+        writer.write("        <tbody>\n");
+
+        for (Member member : memberList) {
+            writer.write("        <tr>\n");
+            writer.write("            <td>" + member.getId() + "</td>\n");
+            writer.write("            <td>" + member.getUsername() + "</td>\n");
+            writer.write("            <td>" + member.getAge() + "</td>\n");
+            writer.write("        </tr>\n");
+        }
+
+        writer.write("        </tbody>\n");
+        writer.write("    </table>\n");
+        writer.write("</body>\n");
+        writer.write("</html>\n");
+
+//        writer.write(sb.toString());
+    }
 }
--- src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -1,2 +1,46 @@
-package hello.servlet.web.servlet;public class MemberSaveServlet {
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("username");
+        String age = req.getParameter("age");
+
+        Member member = new Member(name, Integer.parseInt(age));
+        memberRepository.save(member);
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write(
+                "<html>\n" +
+                        "<head>\n" +
+                        "    <meta charset=\"UTF-8\">\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "    <h1>회원 저장 결과</h1>\n" +
+                        "    <p>아이디: " + member.getId() + "</p>\n" +
+                        "    <p>이름: " + member.getUsername() + "</p>\n" +
+                        "    <p>나이: " + member.getAge() + "</p>\n" +
+                        "    <a href=\"/index.html\">메인</a>\n" +
+                        "    <a href=\"/servlet/members/new-form\">신규 회원 등록</a>\n" +
+                        "    <a href=\"/servlet/members\">회원 목록</a>\n" +
+                        "</body>\n" +
+                        "</html>"
+        );
+
+    }
 }
