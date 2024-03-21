00c76df9fdb8601dc7c946efaf4bfb4dfa7316bd
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-21 11:53:25 +0900

    [3-1] 회원관리 간단 테스트

--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -1,4 +1,62 @@
-import static org.junit.jupiter.api.Assertions.*;
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MemberRepositoryTest {
-  
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("Jbiscode", 29);
+        //when
+        Member savedMember = memberRepository.save(member);
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findById() {
+//given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        Member findMember = memberRepository.findById(member1.getId());
+        //then
+        assertThat(findMember).isEqualTo(member1);
+
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("A", 20);
+        Member member2 = new Member("B", 30);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> memberList = memberRepository.findAll();
+
+        //then
+        assertThat(memberList.size()).isEqualTo(2);
+        assertThat(memberList).contains(member1, member2);
+    }
+
+    @Test
+    void clearStore() {
+    }
 }
