a1cee5979a274750ca73b7431f67e6520de4bbf7
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-22 11:54:50 +0900

    [4-2] 프론트 컨트롤러 도입 V1

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -1,5 +1,8 @@
 package hello.servlet.web.frontcontroller.v1;
 
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,14 +15,31 @@ import java.util.Map;
 
 // v1 하위의 어떤 경로도 이 서블릿이 처리하도록 설정
 @WebServlet(name = "frontControllerV1", urlPatterns = "/front-controller/v1/*")
-public class FrontControllerV1 extends HttpServlet {
-    private Map<String, ControllerV1> controllerV1Map = new HashMap<>();
+public class FrontControllerServletV1 extends HttpServlet {
+    private final Map<String, ControllerV1> controllerMapV1 = new HashMap<>();
 
+    /**
+     * controllerV1Map에 URL 패턴과 해당 URL을 처리할 컨트롤러를 매핑합니다.
+     * "/front-controller/v1/members/new-form" URL 패턴은 MemberFormControllerV1 객체를 통해 처리됩니다.
+     * "/front-controller/v1/members/save" URL 패턴은 MemberSaveControllerV1 객체를 통해 처리됩니다.
+     * "/front-controller/v1/members/members" URL 패턴은 MemberListControllerV1 객체를 통해 처리됩니다.
+     */
     public FrontControllerServletV1(){
-        controllerV1Map.put("/front-conroller/v1/members/new-form", new MemberFormControllerV1());
-    }
+    controllerMapV1.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+    controllerMapV1.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+    controllerMapV1.put("/front-controller/v1/members", new MemberListControllerV1());
+}
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV1.service");
 
+        String requestURI = request.getRequestURI();
+        ControllerV1 controller = controllerMapV1.get(requestURI);
+
+        if(controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        controller.process(request, response);
     }
 }
--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -1,2 +1,19 @@
-package hello.servlet.web.frontcontroller.v1.controller;public class MemberFormControllerV1 {
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        request.getRequestDispatcher(viewPath).forward(request, response);
+    }
 }
